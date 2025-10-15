8e73de507e5ae68c0711a2fc23822654584c98ac
woojinM-git <moonwj5685@gmail.com>
2025-10-15 16:13:54 +0900

    first flyway 적용

---
+++ src/main/resources/db/migration/V6__add_data_to_damin.sql
@@ -0,0 +1,1 @@
+INSERT INTO `checkin`.`admin` (`contentid`, `type`, `id`, `pw`, `status`, `name`, `refToken`, `phone`) VALUES ('1003654', '1', 'admin', '1111', '0', 'woojin', 'NULL', '010-0000-0001');
