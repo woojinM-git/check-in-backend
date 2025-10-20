c6f333f70a4960df68c403a28d5ba6e51d4da74a
junyongseong <seongyongjun1160@gmail.com>
2025-10-20 11:30:54 +0900

    flyway V20추가 그에 맞춰 엔티티및 dto 수정

--- src/main/java/com/sist/backend/dto/hotel/RoomResponse.java
+++ src/main/java/com/sist/backend/dto/hotel/RoomResponse.java
@@ -16,7 +16,7 @@ public class RoomResponse {
     private String contentId;
     private String name;
     private Integer capacity;
-    private BigDecimal basePrice;
+    private Integer basePrice;
     private Boolean refundable;
     private Boolean breakfastIncluded;
     private Boolean smoking;
