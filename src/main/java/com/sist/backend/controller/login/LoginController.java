015d27c3edf5e852214f4ecfd53e2bf3a14cdc57
vicelemon1841 <lsk18415@naver.com>
2025-10-21 17:51:42 +0900

    쿠키/토큰 일단 생성
    front에서 부르기 해보고 확인 해봐야함

--- src/main/java/com/sist/backend/controller/login/LoginController.java
+++ src/main/java/com/sist/backend/controller/login/LoginController.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,10 +15,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sist.backend.entity.Customer;
+import com.sist.backend.jwt.JwtProvider;
 import com.sist.backend.service.CustomerService;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/api/login")
@@ -30,6 +34,56 @@ public class LoginController {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JwtProvider jwtProvider;
+
+
+    @PostMapping("")
+    @Operation(summary="로그인", description="로그인 창에서 입력한 값 가져오기")
+    public ResponseEntity<Map<String, Object>> login(@RequestBody Customer customer, HttpServletResponse response) {
+        Map<String, Object> result = new HashMap<>();
+        Optional<Customer> customer_exist = customerService.findById(customer.getId());
+        if(customer_exist.isPresent()){
+            Customer customer_exist_entity = customer_exist.get();
+            System.out.println("==================아이디 존재!==================");
+            if(passwordEncoder.matches(customer.getPassword(), customer_exist.get().getPassword())){
+                System.out.println("==================로그인 성공!==================");
+                String uuid = UUID.randomUUID().toString();
+                int accessTokenExpireTime = 3600;
+                int refreshTokenExpireTime = 604800;
+                Map<String, Object> payload = new HashMap<>();
+                payload.put("id", customer_exist_entity.getId());
+                String accessToken = jwtProvider.getToken(payload, accessTokenExpireTime);
+                payload.put("tokenID",uuid);
+                payload.put("accessToken",accessToken);
+                String refreshToken = jwtProvider.getToken(payload, refreshTokenExpireTime);
+                Cookie accesscookie = new Cookie("accessToken", accessToken);
+                Cookie refreshcookie = new Cookie("refreshToken", refreshToken);
+
+                // *** HttpOnly 쿠키 설정을 위한 핵심 4가지 ***
+                
+                // 3-1. 토큰 만료 시간 설정 (초 단위). AuthService에서 3600초(1시간)로 설정했으므로 동일하게 설정
+                accesscookie.setMaxAge(accessTokenExpireTime); 
+                refreshcookie.setMaxAge(refreshTokenExpireTime);
+                // 3-2. JavaScript 접근 방지 (HttpOnly 설정 - 보안 강화)
+                accesscookie.setHttpOnly(true); 
+                refreshcookie.setHttpOnly(true);
+                // 3-3. 쿠키 경로 설정 (최상위 경로 '/'). 모든 요청에서 쿠키가 전송되도록 합니다.
+                accesscookie.setPath("/");
+                refreshcookie.setPath("/");
+
+
+                response.addCookie(accesscookie);
+
+                result.put("message","로그인 성공1");
+            }
+        }else{
+            result.put("message","아이디 또는 비밀번호가 일치하지 않습니다");
+        }
+
+        return ResponseEntity.ok(result);
+    }
     
     @PostMapping("/checkId")
     @Operation(summary="아이디 중복 체크", description="아이디만 가져와서 중복 검사하기")
@@ -54,6 +108,7 @@ public class LoginController {
         System.out.println("====================================="+customer+"========================================");
         customer.setPassword(passwordEncoder.encode(customer.getPassword()));
         customer.setJoinDate(LocalDateTime.now());
+        customer.setCash(null);
         customer.setStatus(0);
         customer.setTotalPrice(0);
         customer.setPoint(0);
