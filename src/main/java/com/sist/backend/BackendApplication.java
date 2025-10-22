40383e9112ee7e4a5a5dbec5b92b7c701cc35b21
Taein Jang <80192692+roblej@users.noreply.github.com>
2025-10-22 15:12:59 +0900

    페이지 이탈 시 락 해제, 스케줄링으로 삭제

--- src/main/java/com/sist/backend/BackendApplication.java
+++ src/main/java/com/sist/backend/BackendApplication.java
@@ -3,8 +3,10 @@ package com.sist.backend;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BackendApplication {
 
 	public static void main(String[] args) {
