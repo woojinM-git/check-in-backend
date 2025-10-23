2be3d1acb4cbd2d18e8440f9c45f99c37e464e55
woojinM-git <moonwj5685@gmail.com>
2025-10-23 17:51:11 +0900

    ㅋㅍ

--- src/main/java/com/sist/backend/repository/CouponRepository.java
+++ src/main/java/com/sist/backend/repository/CouponRepository.java
@@ -1,7 +1,11 @@
 package com.sist.backend.repository;
 
 import com.sist.backend.entity.Coupon;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -17,4 +21,11 @@ public interface CouponRepository extends JpaRepository<Coupon, Integer>{
     
     // 상태별 쿠폰 조회
     List<Coupon> findByStatus(Boolean status);
+
+    // 
+    @Query("SELECT c FROM Coupon c " +
+            "LEFT JOIN FETCH c.couponTemplate " +
+            "LEFT JOIN FETCH c.customer " +
+            "WHERE c.adminIdx = :adminIdx")
+    Page<Coupon> findByAdminIdx(Integer adminIdx, Pageable pageable);
 }
