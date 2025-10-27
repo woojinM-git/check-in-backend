aaebfb7fc2034a2aea8169d6a6d215e124484415
woojinM-git <moonwj5685@gmail.com>
2025-10-27 18:58:52 +0900

    ??

--- src/main/java/com/sist/backend/dto/hotel/HotelDraftDto.java
+++ src/main/java/com/sist/backend/dto/hotel/HotelDraftDto.java
@@ -31,3 +31,4 @@ public class HotelDraftDto {
         return dto;
     }
 }
+
--- src/main/java/com/sist/backend/service/HotelDraftService.java
+++ src/main/java/com/sist/backend/service/HotelDraftService.java
@@ -68,3 +68,4 @@ public class HotelDraftService {
         return hotelDraftRepository.existsByAdminIdx(adminIdx);
     }
 }
+
