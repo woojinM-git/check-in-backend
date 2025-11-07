ff52f59aa770dacaf470fbae9e6fb59df7c6db58
woojinM-git <moonwj5685@gmail.com>
2025-11-07 17:03:54 +0900

    11

--- src/main/java/com/sist/backend/entity/HotelInfo.java
+++ src/main/java/com/sist/backend/entity/HotelInfo.java
@@ -30,9 +30,6 @@ public class HotelInfo {
     @Column(name = "adminIdx")
     private Integer adminIdx;
     
-    @Column(name = "count")
-    private Integer count;
-    
     @Column(name = "title")
     private String title;
     
